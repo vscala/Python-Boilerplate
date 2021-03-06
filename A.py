777a8fa8f4113ca0c1e42b482304504214097050
vscala <57908042+vscala@users.noreply.github.com>
2021-03-06 13:41:08 -0500

    Add files via upload

---
+++ A.py
@@ -0,0 +1,16 @@
+#Problem A
+
+import sys
+import math
+from math import gcd,floor,sqrt,log
+
+sys.setrecursionlimit(100000000)
+mod = 1000000007
+
+ISEQ = lambda: map(int(), input().split())
+FSEQ = lambda: map(float(), input().split())
+SSEQ = lambda: input().split()
+INT = lambda: int(input())
+STR = lambda: input()
+
+
