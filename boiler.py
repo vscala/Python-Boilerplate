3b8c7a5c37df885e06a3bb6f750f16854eac4e40
vscala <57908042+vscala@users.noreply.github.com>
2021-03-06 13:58:43 -0500

    Update and rename A.py to boiler.py

--- boiler.py
+++ boiler.py
@@ -1,5 +1,3 @@
-#Problem A
-
 import sys
 import math
 from math import gcd,floor,sqrt,log
@@ -7,10 +5,9 @@ from math import gcd,floor,sqrt,log
 sys.setrecursionlimit(100000000)
 mod = 1000000007
 
-ISEQ = lambda: map(int(), input().split())
-FSEQ = lambda: map(float(), input().split())
+ISEQ = lambda: list(map(int, input().split()))
+FSEQ = lambda: list(map(float, input().split()))
 SSEQ = lambda: input().split()
 INT = lambda: int(input())
 STR = lambda: input()
-
 
